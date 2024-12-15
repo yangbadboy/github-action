6fc4b477601dfd3fb4cf23732c10fa30d8c77009
duconline09 <42961466+duconline09@users.noreply.github.com>
2024-12-15 23:45:18 +0700

    Update required provider version

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0, < 7"
+      version = "~> 4.0.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.60.0, < 7"
+      version = "~> 5.29.0"
     }
   }
   # backend "gcs" {
