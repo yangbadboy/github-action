651b4622a2eb3f3a19a17404802d947dfe51d9be
yang yang <nguyenducgiangrubic@gmail.com>
2024-12-25 00:16:42 +0700

    test

--- terraform/aws_networking.tf
+++ terraform/aws_networking.tf
@@ -11,7 +11,8 @@ data "aws_vpc" "peer" {
   provider = aws.seoul
   id = "vpc-0c445f5ad3d99c7d8"
 }
-resource "aws_vpc_peering_connection" "foo" {
+
+resource "aws_vpc_peering_connection" "test_peering" {
   peer_owner_id = "184136210319"
   peer_vpc_id   = data.aws_vpc.peer.id
   vpc_id        = aws_vpc.aws-vpc-github-action.id
@@ -19,3 +20,16 @@ resource "aws_vpc_peering_connection" "foo" {
   auto_accept   = true
 }
 
+
+data "aws_subnet" "peer" {
+  id = "subnet-0f948b7e031117087"
+}
+data "aws_route_table" "peer" {
+  subnet_id = data.aws_subnet.peer
+}
+
+resource "aws_route" "route" {
+  route_table_id            = data.aws_route_table.selected.id
+  destination_cidr_block    = var.aws_network_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.test_peering.id
+}
