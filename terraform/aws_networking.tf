633d03baa8dc7b53ad7b50065fb936ac803b3ea4
yang yang <nguyenducgiangrubic@gmail.com>
2024-12-29 18:47:47 +0700

    test

--- terraform/aws_networking.tf
+++ terraform/aws_networking.tf
@@ -1,38 +1,38 @@
-resource "aws_vpc" "aws-vpc-github-action" {
-  cidr_block           = var.aws_network_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    "Name" = "aws-vpc-yang1"
-  }
-}
-
-data "aws_vpc" "peer" {
-  provider = aws.seoul
-  id = "vpc-0c445f5ad3d99c7d8"
-}
-
-resource "aws_vpc_peering_connection" "test_peering" {
-  peer_owner_id = "184136210319"
-  peer_vpc_id   = data.aws_vpc.peer.id
-  vpc_id        = aws_vpc.aws-vpc-github-action.id
-  peer_region   = "ap-northeast-2"
-  auto_accept   = true
-}
-
-
-# data "aws_subnet" "peer" {
-#   provider = aws.seoul
-#   id = "subnet-0f948b7e031117087"
-# }
-# data "aws_route_table" "peer" {
-#   provider = aws.seoul
-#   subnet_id = "subnet-0f948b7e031117087"
+# resource "aws_vpc" "aws-vpc-github-action" {
+#   cidr_block           = var.aws_network_cidr
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+#   tags = {
+#     "Name" = "aws-vpc-yang1"
+#   }
 # }
 
-resource "aws_route" "route" {
-  provider = aws.seoul
-  route_table_id            = "rtb-09ddd7dd948ba34c2"
-  destination_cidr_block    = var.aws_network_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.test_peering.id
-}
+# data "aws_vpc" "peer" {
+#   provider = aws.seoul
+#   id = "vpc-0c445f5ad3d99c7d8"
+# }
+
+# resource "aws_vpc_peering_connection" "test_peering" {
+#   peer_owner_id = "184136210319"
+#   peer_vpc_id   = data.aws_vpc.peer.id
+#   vpc_id        = aws_vpc.aws-vpc-github-action.id
+#   peer_region   = "ap-northeast-2"
+#   auto_accept   = true
+# }
+
+
+# # data "aws_subnet" "peer" {
+# #   provider = aws.seoul
+# #   id = "subnet-0f948b7e031117087"
+# # }
+# # data "aws_route_table" "peer" {
+# #   provider = aws.seoul
+# #   subnet_id = "subnet-0f948b7e031117087"
+# # }
+
+# resource "aws_route" "route" {
+#   provider = aws.seoul
+#   route_table_id            = "rtb-09ddd7dd948ba34c2"
+#   destination_cidr_block    = var.aws_network_cidr
+#   vpc_peering_connection_id = aws_vpc_peering_connection.test_peering.id
+# }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,45 @@
-terraform {
-  required_version = ">= 1.3"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0.0"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.29.0"
-    }
-  }
-  # backend "gcs" {
-  #   bucket = "cg-stg-gcp-terraform-state-yang"
-  #   prefix = "terraform/state-files"
-  # }
-  backend "s3" {
-    bucket         = "github-action-yang"       
-    key            = "terraform/state-files/terraform.tfstate" 
-    region         = "us-east-1"               
-    dynamodb_table = "terraform-lock-table"     
-    encrypt        = true                       
-  }
-}
+# terraform {
+#   required_version = ">= 1.3"
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 4.0.0"
+#     }
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.29.0"
+#     }
+#   }
+#   # backend "gcs" {
+#   #   bucket = "cg-stg-gcp-terraform-state-yang"
+#   #   prefix = "terraform/state-files"
+#   # }
+#   backend "s3" {
+#     bucket         = "github-action-yang"       
+#     key            = "terraform/state-files/terraform.tfstate" 
+#     region         = "us-east-1"               
+#     dynamodb_table = "terraform-lock-table"     
+#     encrypt        = true                       
+#   }
+# }
 
 
 
-provider "google" {
-  #credentials = file(var.gcp_credentials_file_path)
+# provider "google" {
+#   #credentials = file(var.gcp_credentials_file_path)
 
-  # Should be able to parse project from credentials file but cannot.
-  # Cannot convert string to map and cannot interpolate within variables.
-  project = "gamehub-stg1"
+#   # Should be able to parse project from credentials file but cannot.
+#   # Cannot convert string to map and cannot interpolate within variables.
+#   project = "gamehub-stg1"
 
-  region = "us-central1"
-}
+#   region = "us-central1"
+# }
 
-provider "aws" {
-  region     = "us-east-1"
-}
+# provider "aws" {
+#   region     = "us-east-1"
+# }
 
-provider "aws" {
-  alias = "seoul"
-  region     = "ap-northeast-2"
-}
+# provider "aws" {
+#   alias = "seoul"
+#   region     = "ap-northeast-2"
+# }
